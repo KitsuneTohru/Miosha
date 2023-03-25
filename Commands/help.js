9283bf2236f02d8b29e2a90715ef71d3872d0b22
KitsuneTohru <mcbachkim206@gmail.com>
2023-03-26 00:24:15 +0700

    Update help.js

--- Commands/help.js
+++ Commands/help.js
@@ -176,7 +176,7 @@ module.exports = {
             .setColor('White')
             .setTitle(`<a:LYG_Sparkle:1084084997398470747> LYG Bot: Trang Chủ <a:LYG_Sparkle:1084084997398470747>`)
             .setAuthor({ name: 'LYG Bot#5189', iconURL: 'https://images-ext-1.discordapp.net/external/dDSr9ZFmlXp54AiCmfU3IxWk3MNZJprYwKOiw6GJdlo/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/1061527111829041242/8d17657d432afefb163bc17ab15af205.png' })
-            .setDescription('<:LYG_FubukiPing1:1084085915368050788> **Phiên Bản:** `[LYG]` v.0.5.0/`[Game Over]` v.0.3.5\n<a:LYG_Clock:1084322030331105370> **Last Update:** <t:1679396400> (<t:1679396400:R>)\n\n**Click Các Nút Bên Dưới Để Xem Thông Tin Nhá!**\n> <:LYG_LaylaHmm:1086172125955633192> **Commands**\n> <a:LYG_OkayuLove:1087692048280334347> **Changelogs**\n> <a:LYG_FubukiWhat:1084085930266218556> **FaQs**\n\n❌**Link Mời Bot (KHÔNG KHẢ DỤNG)**\n🔗[[Support Server Link 1]](https://discord.gg/HPGXJKzhfW)\n🔗[[Support Server Link 2]](https://discord.gg/vfDWAT7xmy)\n**Ghi Chú: Cả 2 Đều Cần Verify Nhé!!!**\n*(Note: Đang Trong Quá Trình Test Nhá...)*')
+            .setDescription('<:LYG_FubukiPing1:1084085915368050788> **Phiên Bản:** `[LYG]` v.0.5.0/`[Game Over]` v.0.3.5\n<a:LYG_Clock:1084322030331105370> **Last Update:** <t:1679762100> (<t:1679762100:R>)\n\n**Click Các Nút Bên Dưới Để Xem Thông Tin Nhá!**\n> <:LYG_LaylaHmm:1086172125955633192> **Commands**\n> <a:LYG_OkayuLove:1087692048280334347> **Changelogs**\n> <a:LYG_FubukiWhat:1084085930266218556> **FaQs**\n\n❌**Link Mời Bot (KHÔNG KHẢ DỤNG)**\n🔗[[Support Server Link 1]](https://discord.gg/HPGXJKzhfW)\n🔗[[Support Server Link 2]](https://discord.gg/vfDWAT7xmy)\n**Ghi Chú: Cả 2 Đều Cần Verify Nhé!!!**\n*(Note: Đang Trong Quá Trình Test Nhá...)*')
             .setImage('https://media.discordapp.net/attachments/993475207828361266/1061636491702435860/png_20221122_230528_0000.png')
             .setTimestamp()
             .setFooter({ text: 'Bot Được Tạo Bởi: Kitsunezi#2905 (2023 - 2023)', iconURL: 'https://cdn.discordapp.com/attachments/962948410472816650/1084078406561443900/Kitsunezi_March_2023.png' });
@@ -262,7 +262,7 @@ module.exports = {
         const collector = interaction.channel.createMessageComponentCollector({ filter, time: 300000 })
         collector.on('collect', async a => {
             if (a.customId === 'commands1' || a.customId === 'commands3' || a.customId === 'commands4') {
-		j = 0
+                j = 0
                 await wait(500)
                 await interaction.editReply({
                     embeds: [CmdEmbed[0]],

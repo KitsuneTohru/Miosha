d86b36112b845804b0ff524bd8fc2e4cc6038860
KitsuneTohru <mcbachkim206@gmail.com>
2023-03-26 22:34:54 +0700

    Update avatar.js

--- Commands/avatar.js
+++ Commands/avatar.js
@@ -7,27 +7,29 @@ module.exports = {
         .addUserOption(option => option.setName('user').setDescription('Avatar Của Người Dùng Cần Show')),
     async execute(interaction) {
         const user = interaction.options.getUser('user');
+        if(user){
         const user_embed = new EmbedBuilder()
             .setAuthor({ name: 'LYG Bot#5189', iconURL: 'https://images-ext-1.discordapp.net/external/dDSr9ZFmlXp54AiCmfU3IxWk3MNZJprYwKOiw6GJdlo/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/1061527111829041242/8d17657d432afefb163bc17ab15af205.png' })
-            .setTitle(`<:LYG_Okayu_Mogu:1089566808719237210> **Avatar Của** ${user}`)
-            .setDescription(`[User Avatar URL](${user.displayAvatarURL({dynamic: true})})`)
+            .setTitle(`<:LYG_Okayu_Mogu:1089566808719237210> **Avatar Displayer**`)
+            .setDescription(`[Avatar URL](${user.displayAvatarURL({dynamic: true})}) **(User: ${user})**`)
             .setColor('Blue')
-            .setImage(`${user.displayAvatarURL({dynamic: true})}`)
-            .seFooter({ text: 'Bot Được Tạo Bởi: Kitsunezi#2905 (2023 - 2023)', iconURL: 'https://cdn.discordapp.com/attachments/962948410472816650/1084078406561443900/Kitsunezi_March_2023.png' });
-        const your_embed = new EmbedBuilder()
-            .setAuthor({ name: 'LYG Bot#5189', iconURL: 'https://images-ext-1.discordapp.net/external/dDSr9ZFmlXp54AiCmfU3IxWk3MNZJprYwKOiw6GJdlo/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/1061527111829041242/8d17657d432afefb163bc17ab15af205.png' })
-            .setTitle(`<:LYG_Okayu_Mogu:1089566808719237210> **Avatar Của Bạn`)
-            .setDescription(`[Avatar URL Của Bạn](${interaction.user.displayAvatarURL({dynamic: true})})`)
-            .setColor('Blue')
-            .setImage(`${interaction.user.displayAvatarURL({dynamic: true})}`)
+            .setImage(`${user.displayAvatarURL({dynamic: true, size: 512})}`)
             .setFooter({ text: 'Bot Được Tạo Bởi: Kitsunezi#2905 (2023 - 2023)', iconURL: 'https://cdn.discordapp.com/attachments/962948410472816650/1084078406561443900/Kitsunezi_March_2023.png' });
- 
-        if (user) 
-            return interaction.reply({
+        await interaction.reply({
                 embeds: [user_embed]
             });
-        return interaction.reply({
-            embeds: [your_embed]
-        });
-    },    
-};
+        }
+        else {
+        const your_embed = new EmbedBuilder()
+            .setAuthor({ name: 'LYG Bot#5189', iconURL: 'https://images-ext-1.discordapp.net/external/dDSr9ZFmlXp54AiCmfU3IxWk3MNZJprYwKOiw6GJdlo/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/1061527111829041242/8d17657d432afefb163bc17ab15af205.png' })
+            .setTitle(`<:LYG_Okayu_Mogu:1089566808719237210> **Avatar Displayer**`)
+            .setDescription(`[Avatar URL](${interaction.user.displayAvatarURL({dynamic: true})}) **(Bạn: ${interaction.user})**`)
+            .setColor('Red')
+            .setImage(`${interaction.user.displayAvatarURL({dynamic: true,size: 512})}`)
+            .setFooter({ text: 'Bot Được Tạo Bởi: Kitsunezi#2905 (2023 - 2023)', iconURL: 'https://cdn.discordapp.com/attachments/962948410472816650/1084078406561443900/Kitsunezi_March_2023.png' });
+        await interaction.reply({
+                embeds: [your_embed]
+            });
+        }     
+    }
+}

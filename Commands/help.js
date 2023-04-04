85db559d60f49232899e23f9f45478f675a525bb
KitsuneTohru <mcbachkim206@gmail.com>
2023-04-04 19:09:45 +0700

    Update help.js

--- Commands/help.js
+++ Commands/help.js
@@ -203,26 +203,17 @@ module.exports = {
             '#D3FFB3',
             'FF90000',
         ]
-        const imgarr = [
-            'https://media.discordapp.net/attachments/993475207828361266/1061636491702435860/png_20221122_230528_0000.png',
-            '',
-            '',
-            '',
-            '',
-        ]
         const CmdEmbed = []
         var i, j = 0
         for (i = 0; i < 5; i++) {
             const title = titlearr[i]
             const color = colorarr[i]
             const desc = descarr[i]
-            const img = imgarr[i]
             CmdEmbed[i] = new EmbedBuilder()
                 .setAuthor({ name: 'LYG Bot#5189', iconURL: 'https://images-ext-1.discordapp.net/external/dDSr9ZFmlXp54AiCmfU3IxWk3MNZJprYwKOiw6GJdlo/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/1061527111829041242/8d17657d432afefb163bc17ab15af205.png' })
                 .setColor(color)
                 .setTitle(title)
                 .setDescription(desc)
-                .setImage(img)
                 .setFooter({ text: 'Bot Được Tạo Bởi: Kitsunezi#2905 (2023 - 2023)', iconURL: 'https://cdn.discordapp.com/attachments/962948410472816650/1084078406561443900/Kitsunezi_March_2023.png' })
                 .setTimestamp()
         }

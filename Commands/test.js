12776f08426b9362e7f65a7767183c9e8770b33c
KitsuneTohru <mcbachkim206@gmail.com>
2023-04-04 19:21:37 +0700

    Update test.js

--- Commands/test.js
+++ Commands/test.js
@@ -13,7 +13,7 @@ module.exports = {
             .setImage('https://media.discordapp.net/attachments/993475207828361266/1061636491702435860/png_20221122_230528_0000.png')
             .setTimestamp()
             .setFooter({ text: 'Bot Được Tạo Bởi: Kitsunezi#2905 (2023 - 2023)', iconURL: 'https://cdn.discordapp.com/attachments/962948410472816650/1084078406561443900/Kitsunezi_March_2023.png' });
-        var check = undefined
+        var check = false
         const userarr = ['751225225047179324', '809259609700302935', '927221951439700058', '786816081032773662', '729671009631862834', '1084992470074531960']
         function checkID(user) {
             var i
@@ -21,7 +21,6 @@ module.exports = {
                 if (user === userarr[i])
                     check = true
             }
-            check = false
         }
         checkID(user)
         if (check === false) {

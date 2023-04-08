c9db12a96efab50fa32875735fb13ffd36d0ed94
KitsuneTohru <mcbachkim206@gmail.com>
2023-04-08 13:25:00 +0700

    Update coinflip.js

--- Commands/coinflip.js
+++ Commands/coinflip.js
@@ -107,7 +107,6 @@ module.exports = {
             .setTitle(`<a:LYG_Clock:1084322030331105370> **Command - Cooldown**`)
             .setAuthor({ name: 'Miosha#5189', iconURL: 'https://cdn.discordapp.com/attachments/1016930426520084560/1093948954690986094/20230408_002020_0000.png' })
             .setDescription(`<:LYG_FubukiPing1:1084085915368050788> | <@${cduser}> Oi! Bạn Phải Chờ Đến <t:${Math.round(cdend[cduser] / 1000)}> (<t:${Math.round(cdend[cduser] / 1000)}:R>) Mới Có Thể Thực Hiện Lệnh Nhé!`)
-            .setImage('https://media.discordapp.net/attachments/993475207828361266/1061636491702435860/png_20221122_230528_0000.png')
             .setTimestamp()
             .setFooter({ text: 'Bot Được Tạo Bởi: Kitsunezi#2905 (2023 - 2023)', iconURL: 'https://cdn.discordapp.com/attachments/962948410472816650/1084078406561443900/Kitsunezi_March_2023.png' });
         if (cd.has(interaction.user.id)) {
